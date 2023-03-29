816b5ca0bb43f5a1fdc2f330afcca0b24cb5c411
Jose Gutierrez <112657079+Elcrackje@users.noreply.github.com>
2023-03-29 18:38:44 -0500

    Add files via upload

---
+++ TB1_Matedis.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
+
+void GenderedWheatherProbability(float** matrix);
+void ShowMatrix(float** matrix);
+int genNumber();
+void printNums(int n);
+
+using namespace std;
+using namespace System;
+
+int main() {
+
+	srand(time(NULL));
+
+	float** weather_matrix = new float* [4];
+
+	for (int i = 0; i < 4; ++i) {
+		weather_matrix[i] = new float[4];
+	}
+
+	GenderedWheatherProbability(weather_matrix);
+	ShowMatrix(weather_matrix);
+
+	int n = genNumber();
+	printNums(n);
+
+	system("pause");
+
+	for (int i = 0; i < 4; ++i)
+		delete[] weather_matrix[i];
+	delete[] weather_matrix;
+
+	
+
+	return 0;
+}
+// funcion para obtener un numero aleatorio entre 60 y 90
+int genNumber()
+{
+	srand(time(NULL));
+	int n = rand() % 31 + 60;
+	return n;
+}
+// funcion para imprimir los numeros
+void printNums(int n)
+{
+	for (int i = 0; i < 4; ++i) {
+		cout << n + i << " ";
+	}
+	cout << endl;
+}
+
+
+void GenderedWheatherProbability(float** matrix) {
+	int num1, num2, num3, num4;
+
+	for (int j = 0; j < 4; j++) {
+
+		num1 = 1 + rand() % 50;
+		num2 = 1 + rand() % (60 - num1);
+		num3 = 1 + rand() % (70 - num1 - num2);
+		num4 = 100 - num1 - num2 - num3;
+
+		matrix[0][j] = num1;
+		matrix[1][j] = num2;
+		matrix[2][j] = num3;
+		matrix[3][j] = num4;
+	}
+}
+
+void ShowMatrix(float** matrix) {
+	int aux = 0;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+
+			aux = matrix[i][j];
+			if (aux % 10 == 0) {
+				cout << "[" << (float)matrix[i][j] / 100 << "0]";
+			}
+			else {
+				cout << "[" << (float)matrix[i][j] / 100 << "]";
+			}
+		}
+		cout << endl;
+	}
+}
