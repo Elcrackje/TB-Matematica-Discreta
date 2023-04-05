dd1039bd14ed3ac99d21c4c57b7ce54c33d7e62b
Jose Gutierrez <112657079+Elcrackje@users.noreply.github.com>
2023-04-04 23:09:01 -0500

    Add files via upload

--- TB1_Matedis.cpp
+++ TB1_Matedis.cpp
@@ -2,58 +2,67 @@
 #include <cstdlib>
 #include <ctime>
 
-void GenderedWheatherProbability(float** matrix);
-void ShowMatrix(float** matrix);
-int genNumber();
-void printNums(int n);
+void RegisterNumber(int* number); // Beth
+void WheatherProbabilityGenerator(double** matrix); // Sebas
+void ProbabilityMatrix(double** matrix); // Jose
+void ShowProbMatrix(double** matrix); // Jose
+void ShowMatrix(double** matrix); // Diego
 
 using namespace std;
 using namespace System;
 
-int main() {
+int main() { // Mathias
 
 	srand(time(NULL));
 
-	float** weather_matrix = new float* [4];
+	// double** weather_matrix = new double* [4];
+	// int* day_number = new int;
+	double** probability_matrix = new double* [4];
 
-	for (int i = 0; i < 4; ++i) {
-		weather_matrix[i] = new float[4];
+	/*
+	for (int i = 0; i < 4; i++) {
+		weather_matrix[i] = new double[4];
+	}
+	*/
+	for (int i = 0; i < 4; i++) {
+		probability_matrix[i] = new double[4];
 	}
 
-	GenderedWheatherProbability(weather_matrix);
-	ShowMatrix(weather_matrix);
+	// RegisterNumber(day_number);
+	// WheatherProbabilityGenerator(weather_matrix);
+	ProbabilityMatrix(probability_matrix);
 
-	int n = genNumber();
-	printNums(n);
+	// ShowMatrix(weather_matrix);
+	ShowProbMatrix(probability_matrix);
 
 	system("pause");
 
-	for (int i = 0; i < 4; ++i)
+	/*
+	delete day_number;
+	for (int i = 0; i < 4; i++)
 		delete[] weather_matrix[i];
 	delete[] weather_matrix;
-
-	
-
+	*/
+	for (int i = 0; i < 4; i++)
+		delete[] probability_matrix[i];
 	return 0;
 }
-// funcion para obtener un numero aleatorio entre 60 y 90
-int genNumber()
-{
-	srand(time(NULL));
-	int n = rand() % 31 + 60;
-	return n;
-}
-// funcion para imprimir los numeros
-void printNums(int n)
-{
-	for (int i = 0; i < 4; ++i) {
-		cout << n + i << " ";
-	}
-	cout << endl;
+
+void RegisterNumber(int* number) {
+	do {
+		cout << "Registre el número de días: "; cin >> *number;
+		system("cls");
+
+		if (*number < 60 || *number > 90) {
+			cout << "Registre un número >= 60 o <= 90" << endl;
+
+			system("pause");
+			system("cls");
+		}
+	} while (*number < 60 || *number > 90);
 }
 
-
-void GenderedWheatherProbability(float** matrix) {
+void WheatherProbabilityGenerator(double** matrix) {
 	int num1, num2, num3, num4;
 
 	for (int j = 0; j < 4; j++) {
@@ -70,18 +79,68 @@ void GenderedWheatherProbability(float** matrix) {
 	}
 }
 
-void ShowMatrix(float** matrix) {
-	int aux = 0;
+void ProbabilityMatrix(double** matrix) {
+	int num1, num2, num3, num4;
+	int suma = 0;
+
+	for (int j = 0; j < 1; j++) {
+
+		num1 = 1 + rand() % 50;
+		num2 = 1 + rand() % (60 - num1);
+		num3 = 1 + rand() % (70 - num1 - num2);
+		num4 = 100 - num1 - num2 - num3;
+
+		matrix[0][j] = num1;
+		matrix[1][j] = num2;
+		matrix[2][j] = num3;
+		matrix[3][j] = num4;
+
+		suma += num1 + num2 + num3 + num4;
+	}
 
 	for (int i = 0; i < 4; i++) {
+		matrix[i][0] = matrix[i][0] / suma;
+	}
+	
+}
+
+void ShowProbMatrix(double** matrix) {
+	for (int i = 0; i < 4; i++) {
+		cout << "[" << matrix[i][0] << "]" << endl;
+	}
+}
+
+void ShowMatrix(double** matrix) {
+	int aux = 0;
+
+	cout << "Nublado = Nu | Parcialmente Nublado = PN | Parcialmente Soleado = PS | Soleado = So" << endl;
+
+	cout << "\n\tNu\tPN\tPS\tSo" << endl;
+
+	for (int i = 0; i < 4; i++) {
+
+		switch (i) {
+		case 0:
+			cout << "Nu\t";
+			break;
+		case 1:
+			cout << "PN\t";
+			break;
+		case 2:
+			cout << "PS\t";
+			break;
+		case 3:
+			cout << "So\t";
+			break;
+		}
 		for (int j = 0; j < 4; j++) {
 
 			aux = matrix[i][j];
 			if (aux % 10 == 0) {
-				cout << "[" << (float)matrix[i][j] / 100 << "0]";
+				cout << "[" << (double)matrix[i][j] / 100 << "0]\t";
 			}
 			else {
-				cout << "[" << (float)matrix[i][j] / 100 << "]";
+				cout << "[" << (double)matrix[i][j] / 100 << "]\t";
 			}
 		}
 		cout << endl;
