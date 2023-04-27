0f9fc3e07b7376002c9dc824fd997698927313dd
Jose Gutierrez <112657079+Elcrackje@users.noreply.github.com>
2023-04-27 12:02:03 -0500

    Update TB1_Entrega 2_ Matematica discreta.cpp

--- TB1_Entrega 2_ Matematica discreta.cpp
+++ TB1_Entrega 2_ Matematica discreta.cpp
@@ -9,11 +9,13 @@ void Conclusion();
 void Credits();
 
 void RegisterNumber(int* number);
-void WheatherProbabilityGenerator(double** matrix);
-void ProbabilityMatrix(double** matrix);
+void RegisterProbabilityMatrix(double** matrix);
+void WheatherProbabilityGenerator(double** matrix, int* number);
 
-void PowMatrix(double** matrix, double** aux, int* number);
-void MultiplyMatrix(double** matrix, double** aux, double** Pmatrix);
+void PowMatrix(double** matrix, double** aux, double** m1, double** m2, double** m3, double** m4);
+void MultiplyMatrix(double** m1, double** m2, double** m3, double** m4, double** Pmatrix, double** auxPmatrix, double** pw1, double** pw2, double** pw3, double** pw4);
+
+void FinalMessage(int* number, double** pw1, double** pw2, double** pw3, double** pw4);
 
 void ShowProbMatrix(double** matrix);
 void ShowMatrix(double** matrix);
@@ -26,21 +28,43 @@ int main() {
 	setlocale(LC_ALL, "spanish");
 	srand(time(NULL));
 
+	int* opcion = new int;
+	int* day_number = new int;
+
 	double** weather_matrix = new double* [4];
 	double** aux_matrix = new double* [4];
+	
+	double** weather1 = new double* [4];
+	double** weather2 = new double* [4];
+	double** weather3 = new double* [4];
+	double** weather4 = new double* [4];
+
+	
 	double** probability_matrix = new double* [4];
 	double** aux_probability_matrix = new double* [4];
+
+	double** PW1 = new double* [4];
+	double** PW2 = new double* [4];
+	double** PW3 = new double* [4];
+	double** PW4 = new double* [4];
 
 	for (int i = 0; i < 4; i++) {
 		weather_matrix[i] = new double[4];
 		aux_matrix[i] = new double[4];
+
+		weather1[i] = new double[4];
+		weather2[i] = new double[4];
+		weather3[i] = new double[4];
+		weather4[i] = new double[4];
 	}
 	for (int i = 0; i < 4; i++) {
 		probability_matrix[i] = new double[1];
 		aux_probability_matrix[i] = new double[1];
+		PW1[i] = new double[1];
+		PW2[i] = new double[1];
+		PW3[i] = new double[1];
+		PW4[i] = new double[1];
 	}
-	int* opcion = new int;
-	int* day_number = new int;
 
 	while (true) {
 		menu(opcion);
@@ -54,17 +78,23 @@ int main() {
 				}
 			}
 			RegisterNumber(day_number);
-			WheatherProbabilityGenerator(weather_matrix);
-			ProbabilityMatrix(probability_matrix);
+			RegisterProbabilityMatrix(probability_matrix);
+			WheatherProbabilityGenerator(weather_matrix, day_number);
 
 			ShowMatrix(weather_matrix);
 			ShowProbMatrix(probability_matrix);
 
-			PowMatrix(weather_matrix, aux_matrix, day_number);
-			MultiplyMatrix(weather_matrix, probability_matrix, aux_probability_matrix);
+			PowMatrix(weather_matrix, aux_matrix, weather1, weather2, weather3, weather4);
+			MultiplyMatrix(weather1, weather2, weather3, weather4, probability_matrix, aux_probability_matrix, PW1, PW2, PW3, PW4);
 
-			ShowMatrix(weather_matrix);
-			ShowProbMatrix(aux_probability_matrix);
+
+			ShowProbMatrix(PW1);
+			ShowProbMatrix(PW2);
+			ShowProbMatrix(PW3);
+			ShowProbMatrix(PW4);
+			cout << endl;
+
+			FinalMessage(day_number, PW1, PW2, PW3, PW4);
 
 			system("pause");
 			break;
@@ -83,9 +113,13 @@ int main() {
 		delete[] weather_matrix[i];
 		delete[] aux_matrix[i];
 		delete[] probability_matrix[i];
-		delete[]aux_probability_matrix[i];
+		delete[] aux_probability_matrix[i];
+		delete[] weather1[i];
+		delete[] weather2[i];
+		delete[] weather3[i];
+		delete[] weather4[i];
 	}
-	delete[] weather_matrix, aux_matrix, probability_matrix, aux_probability_matrix;
+	delete[] weather_matrix, aux_matrix, probability_matrix, aux_probability_matrix, weather1, weather2, weather3, weather4;
 	delete opcion, day_number;
 
 	return 0;
@@ -117,79 +151,260 @@ void menu(int *opcion){
 
 void RegisterNumber(int* number) {
 	do {
-		cout << "Registre el numero de dias: "; cin >> *number;
+		cout << "Registre el número de dias: "; cin >> *number;
 		system("cls");
 
 		if (*number < 60 || *number > 90) {
-			cout << "Registre un numero >= 60 o <= 90" << endl;
+			cout << "Registre un número >= 60 o <= 90" << endl;
 
 			system("pause");
 			system("cls");
 		}
-	} while (*number < 0 || *number > 90);
+	} while (*number < 60 || *number > 90);
 }
 
-void WheatherProbabilityGenerator(double** matrix) {
-	int num1, num2, num3, num4;
+void RegisterProbabilityMatrix(double** matrix) {
+	int num;
 
-	for (int j = 0; j < 4; j++) {
+	do {
+		cout << "\t|Climas|" << endl;
+		cout << "1 -> Nublado\n2 -> Parcialmente nublado\n3 -> Parcialmente soleado\n4 -> Soleado\n" << endl;
+		
+		cout << "Ingrese el número que representa el clima actual: "; cin >> num;
+		system("cls");
 
-		num1 = rand() % 50;
-		num2 = rand() % (60 - num1);
-		num3 = rand() % (70 - num1 - num2);
-		num4 = 100 - num1 - num2 - num3;
+		if (num < 1 || num > 4) {
+			cout << "Registre un número >= 1 o <= 4" << endl;
 
-		matrix[0][j] = (double)num1 / 100.00;
-		matrix[1][j] = (double)num2 / 100.00;
-		matrix[2][j] = (double)num3 / 100.00;
-		matrix[3][j] = (double)num4 / 100.00;
+			system("pause");
+			system("cls");
+		}
+	} while (num < 1 || num > 4);
+
+	switch (num) {
+	case 1:
+		matrix[0][0] = 1;
+		matrix[1][0] = 0;
+		matrix[2][0] = 0;
+		matrix[3][0] = 0;
+		break;
+	case 2:
+		matrix[0][0] = 0;
+		matrix[1][0] = 1;
+		matrix[2][0] = 0;
+		matrix[3][0] = 0;
+		break;
+	case 3:
+		matrix[0][0] = 0;
+		matrix[1][0] = 0;
+		matrix[2][0] = 1;
+		matrix[3][0] = 0;
+		break;
+	case 4:
+		matrix[0][0] = 0;
+		matrix[1][0] = 0;
+		matrix[2][0] = 0;
+		matrix[3][0] = 1;
+		break;
 	}
 }
 
-void ProbabilityMatrix(double** matrix) {
+void WheatherProbabilityGenerator(double** matrix, int* number) {
 	int num1, num2, num3, num4;
 
-	for (int j = 0; j < 1; j++) {
+	if (*number >= 60 && *number <= 69) {
+		for (int j = 0; j < 4; j++) {
 
-		num1 = rand() % 50;
-		num2 = rand() % (60 - num1);
-		num3 = rand() % (70 - num1 - num2);
-		num4 = 100 - num1 - num2 - num3;
+			num4 = 60 + rand()% 25;//s
+			num3 = rand() % (100 - num4);// ps
+			num2 = rand() % (100 - num4 - num3);// pn
+			num1 = 100 - num4 - num3 - num2;// n
 
-		matrix[0][j] = (double)num1 / 100.00;
-		matrix[1][j] = (double)num2 / 100.00;
-		matrix[2][j] = (double)num3 / 100.00;
-		matrix[3][j] = (double)num4 / 100.00;
+			matrix[0][j] = (double)num1 / 100.00;
+			matrix[1][j] = (double)num2 / 100.00;
+			matrix[2][j] = (double)num3 / 100.00;
+			matrix[3][j] = (double)num4 / 100.00;
+		}
+	}
+	else if (*number >= 70 && *number <= 79) {
+		for (int j = 0; j < 4; j++) {
+
+			num4 = 40 + rand() % 25;//s
+			num3 = rand() % (100 - num4);// ps
+			num2 = rand() % (100 - num4 - num3);// pn
+			num1 = 100 - num4 - num3 - num2;// n
+
+			matrix[0][j] = (double)num1 / 100.00;
+			matrix[1][j] = (double)num2 / 100.00;
+			matrix[2][j] = (double)num3 / 100.00;
+			matrix[3][j] = (double)num4 / 100.00;
+		}
+	}
+	else if (*number >= 80 && *number <= 90) {
+		for (int j = 0; j < 4; j++) {
+
+			num4 = 25 + rand() % 25;//s
+			num3 = rand() % (100 - num4);// ps
+			num2 = rand() % (100 - num4 - num3);// pn
+			num1 = 100 - num4 - num3 - num2;// n
+
+			matrix[0][j] = (double)num1 / 100.00;
+			matrix[1][j] = (double)num2 / 100.00;
+			matrix[2][j] = (double)num3 / 100.00;
+			matrix[3][j] = (double)num4 / 100.00;
+		}
 	}
 }
 
-void PowMatrix(double** matrix, double** aux, int* number) {
-	int day_number = 0;
-	//*number = 20;
-
-	for (int k = 1; k < *number; k++) {
+void PowMatrix(double** matrix, double** aux, double** m1, double** m2, double** m3, double** m4) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m1[i][j] = matrix[i][j];
+			m2[i][j] = matrix[i][j];
+			m3[i][j] = matrix[i][j];
+			m4[i][j] = matrix[i][j];
+		}
+	}
+	//m2
+	for (int k = 1; k < 2; k++) {
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				aux[i][j] = 0;
 				for (int p = 0; p < 4; p++) {
-					aux[i][j] += matrix[i][p] * matrix[p][j];
+					aux[i][j] += m2[i][p] * m2[p][j];
 				}
 			}
 		}
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				matrix[i][j] = aux[i][j];
+				m2[i][j] = aux[i][j];
+			}
+		}
+	}
+	//m3
+	for (int k = 1; k < 3; k++) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				aux[i][j] = 0;
+				for (int p = 0; p < 4; p++) {
+					aux[i][j] += m3[i][p] * m3[p][j];
+				}
+			}
+		}
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				m3[i][j] = aux[i][j];
+			}
+		}
+	}
+	//m3
+	for (int k = 1; k < 4; k++) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				aux[i][j] = 0;
+				for (int p = 0; p < 4; p++) {
+					aux[i][j] += m4[i][p] * m4[p][j];
+				}
+			}
+		}
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				m4[i][j] = aux[i][j];
 			}
 		}
 	}
 }
 
-void MultiplyMatrix(double** matrix, double** Pmatrix, double** auxPmatrix) {
+void MultiplyMatrix(double** m1, double** m2, double** m3, double** m4, double** Pmatrix, double** auxPmatrix, double** pw1, double** pw2, double** pw3, double** pw4) {
 	for (int i = 0; i < 4; i++) {
-		auxPmatrix[i][0] = 0;
+		pw1[i][0] = Pmatrix[i][0];
+		pw2[i][0] = Pmatrix[i][0];
+		pw3[i][0] = Pmatrix[i][0];
+		pw4[i][0] = Pmatrix[i][0];
+	}
+
+	//m1
+	for (int i = 0; i < 4; i++) {
+		pw1[i][0] = 0;
 		for (int j = 0; j < 4; j++) {
-			auxPmatrix[i][0] += matrix[i][j] * Pmatrix[j][0];
+			pw1[i][0] += m1[i][j] * Pmatrix[j][0];
 		}
+	}
+	//m2
+	for (int i = 0; i < 4; i++) {
+		pw2[i][0] = 0;
+		for (int j = 0; j < 4; j++) {
+			pw2[i][0] += m2[i][j] * Pmatrix[j][0];
+		}
+	}
+	//m3
+	for (int i = 0; i < 4; i++) {
+		pw3[i][0] = 0;
+		for (int j = 0; j < 4; j++) {
+			pw3[i][0] += m3[i][j] * Pmatrix[j][0];
+		}
+	}
+	//m4
+	for (int i = 0; i < 4; i++) {
+		pw4[i][0] = 0;
+		for (int j = 0; j < 4; j++) {
+			pw4[i][0] += m4[i][j] * Pmatrix[j][0];
+		}
+	}
+}
+
+void FinalMessage(int* number, double** pw1, double** pw2, double** pw3, double** pw4) {
+	//m1
+	if (pw1[0][0] > pw1[1][0] && pw1[0][0] > pw1[2][0] && pw1[0][0] > pw1[3][0]) {
+		cout << "Para el día " << *number + 1 << " el clima es NUBLADO" << endl;
+	}
+	else if (pw1[1][0] > pw1[0][0] && pw1[1][0] > pw1[2][0] && pw1[1][0] > pw1[3][0]) {
+		cout << "Para el día " << *number + 1 << " el clima es PARCIALMENTE NUBLADO" << endl;
+	}
+	else if (pw1[2][0] > pw1[0][0] && pw1[2][0] > pw1[1][0] && pw1[2][0] > pw1[3][0]) {
+		cout << "Para el día " << *number + 1 << " el clima es PARCIALMENTE SOLEADO" << endl;
+	}
+	else if (pw1[3][0] > pw1[0][0] && pw1[3][0] > pw1[1][0] && pw1[3][0] > pw1[2][0]) {
+		cout << "Para el día " << *number + 1 << " el clima es SOLEADO" << endl;
+	}
+	//m2
+	if (pw2[0][0] > pw2[1][0] && pw2[0][0] > pw2[2][0] && pw2[0][0] > pw2[3][0]) {
+		cout << "Para el día " << *number + 2 << " el clima es NUBLADO" << endl;
+	}
+	else if (pw2[1][0] > pw2[0][0] && pw2[1][0] > pw2[2][0] && pw2[1][0] > pw2[3][0]) {
+		cout << "Para el día " << *number + 2 << " el clima es PARCIALMENTE NUBLADO" << endl;
+	}
+	else if (pw2[2][0] > pw2[0][0] && pw2[2][0] > pw2[1][0] && pw2[2][0] > pw2[3][0]) {
+		cout << "Para el día " << *number + 2 << " el clima es PARCIALMENTE SOLEADO" << endl;
+	}
+	else if (pw2[3][0] > pw2[0][0] && pw2[3][0] > pw2[1][0] && pw2[3][0] > pw2[2][0]) {
+		cout << "Para el día " << *number + 2 << " el clima es SOLEADO" << endl;
+	}
+	//m3
+	if (pw3[0][0] > pw3[1][0] && pw3[0][0] > pw3[2][0] && pw3[0][0] > pw3[3][0]) {
+		cout << "Para el día " << *number + 3 << " el clima es NUBLADO" << endl;
+	}
+	else if (pw3[1][0] > pw3[0][0] && pw3[1][0] > pw3[2][0] && pw3[1][0] > pw3[3][0]) {
+		cout << "Para el día " << *number + 3 << " el clima es PARCIALMENTE NUBLADO" << endl;
+	}
+	else if (pw3[2][0] > pw3[0][0] && pw3[2][0] > pw3[1][0] && pw3[2][0] > pw3[3][0]) {
+		cout << "Para el día " << *number + 3 << " el clima es PARCIALMENTE SOLEADO" << endl;
+	}
+	else if (pw3[3][0] > pw3[0][0] && pw3[3][0] > pw3[1][0] && pw3[3][0] > pw3[2][0]) {
+		cout << "Para el día " << *number + 3 << " el clima es SOLEADO" << endl;
+	}
+	//m4
+	if (pw4[0][0] > pw4[1][0] && pw4[0][0] > pw4[2][0] && pw4[0][0] > pw4[3][0]) {
+		cout << "Para el día " << *number + 4 << " el clima es NUBLADO" << endl;
+	}
+	else if (pw4[1][0] > pw4[0][0] && pw4[1][0] > pw4[2][0] && pw4[1][0] > pw4[3][0]) {
+		cout << "Para el día " << *number + 4 << " el clima es PARCIALMENTE NUBLADO" << endl;
+	}
+	else if (pw4[2][0] > pw4[0][0] && pw4[2][0] > pw4[1][0] && pw4[2][0] > pw4[3][0]) {
+		cout << "Para el día " << *number + 4 << " el clima es PARCIALMENTE SOLEADO" << endl;
+	}
+	else if (pw4[3][0] > pw4[0][0] && pw4[3][0] > pw4[1][0] && pw4[3][0] > pw4[2][0]) {
+		cout << "Para el día " << *number + 4 << " el clima es SOLEADO" << endl;
 	}
 }
 
@@ -252,7 +467,6 @@ void Conclusion() {
 	cout << "- El trabajo ha sido un poco difícil, pero ha sido muy útil para" << endl;
 	cout << "  comprender conceptos avanzados de informática y matemáticas.\n" << endl;
 	
-
 	cout << "- La programación en equipo es beneficiosa, ya que el código" << endl;
 	cout << "  permitirá la validación de los días solicitados por el usuario" << endl;
 	cout << "  y la creación de una matriz para mostrar los resultados, utilizando" << endl;
@@ -276,7 +490,7 @@ void Credits() {
 
 	Console::ForegroundColor = ConsoleColor::Red;
 	cout << "Universidad Peruana de Ciencias Aplicadas" << endl;
-	cout << "Ingeniería de Software y Ciencias de la Computación - Ciclo III" << endl;
+	cout << "Ingeniería de Software e Ingeniería de Ciencias de la Computación - Ciclo III" << endl;
 	cout << "matemática Discreta - SS38" << endl;
 	cout << "Antonio Marcos Medina Martinez" << endl;
 
